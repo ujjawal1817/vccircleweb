5f1cfc0a8ae11d15dcfec41843c846d7d4d64139
unknown <ujjawal.srivastava@sparkeighteen.com>
2022-08-09 18:41:09 +0530

    update

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -7,7 +7,7 @@ export default function Card(props) {
     <div className={props.clsName}>
       <div className={props.flxCls}>
         <div className={props.imgVccls}>
-          <Link to={props?.imgSlug || ""}>
+          <Link to={props?.imgSlug || "/"}>
             <img
               src={props.src}
               alt=""
@@ -19,24 +19,24 @@ export default function Card(props) {
         </div>
         <div className={props.sectionDetails}>
           <p className="card-title">
-            <Link to={props?.slughead || ""}>{props.heading}</Link>
+            <Link to={props?.slughead || "/"}>{props.heading}</Link>
           </p>
 
           <p className={props.cardDes}>
-            <Link to={props?.slug || ""} className="colr">
+            <Link to={props?.slug || "/"} className="colr">
               {props.title}
             </Link>
           </p>
            <ul className="author-flx">
           <li className="card-author">
             <span>{moment(props.publish).format("Do MMM YYYY")} . </span>
-            <Link to={props?.slugauth || ""} className="authcolr">
+            <Link to={props?.slugauth || "/"} className="authcolr">
               
               {props.author}
             </Link>
           </li>
           <li >
-            <Link to={props?.slugauth || ""} className={props.tooltipcls}>
+            <Link to={props?.slugauth || "/"} className={props.tooltipcls}>
               {props.sname}
             <span className="tooltiptext">{props.authorsecond}</span>
             </Link>
