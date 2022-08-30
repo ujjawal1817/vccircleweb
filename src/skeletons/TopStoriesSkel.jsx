ca0f3f680479ad9173e778d691f0b0fbe13adec9
ujjawal1817 <ujjawal.srivastava@sparkeighteen.com>
2022-08-30 17:33:14 +0530

    updates

--- src/skeletons/TopStoriesSkel.jsx
+++ src/skeletons/TopStoriesSkel.jsx
@@ -7,49 +7,49 @@ import Advertisement from '../Components/Advertisement'
 export default function TopStoriesSkel() {
     return (
         <>
-        <Advertisement
-                
+            <Advertisement
+
                 width="100%"
                 advBg="advertisement-bg-top adskel"
 
             />
-        <div className='container'>
-            <div className='section-detail'>
-                <div className='sub-sec-one'>
-                    <div className="heading"></div>
-                    <div className="title"></div>
-                    <div className="desc"></div>
-                    <div className='author-details'>
-                        <span className='date'></span>
-                        <span className='author-name'></span>
+            <div className='container'>
+                <div className='section-detail'>
+                    <div className='sub-sec-one'>
+                        <div className="heading"></div>
+                        <div className="title"></div>
+                        <div className="desc"></div>
+                        <div className='author-details'>
+                            <span className='date'></span>
+                            <span className='author-name'></span>
+                        </div>
+                        <div className='btn-skel'></div>
                     </div>
-                    <div className='btn-skel'></div>
-                </div>
-                <div className='sub-sec-one'>
-                    <div className="sec-img">
-                        <img className='img' alt="" />
+                    <div className='sub-sec-one'>
+                        <div className="sec-img">
+                            <img className='img' alt="" />
+                        </div>
                     </div>
+
                 </div>
 
+
+                <div className="cards">
+                    {
+                        [1, 2, 3, 4].map((n) => {
+                            return (
+                                <>
+                                    <div>
+                                        <CardsSkel />
+                                    </div>
+
+                                </>
+                            )
+                        })
+                    }
+
+                </div>
             </div>
-
-
-            <div className="cards">
-                {
-                    [1, 2, 3, 4].map((n) => {
-                        return (
-                            <>
-                                <div>
-                                    <CardsSkel />
-                                </div>
-
-                            </>
-                        )
-                    })
-                }
-
-            </div>
-        </div>
         </>
     )
 }
